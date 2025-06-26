cb8bce5b1a02361c4929785eca75a0529e78c43b
𝐁𝐚𝐘𝐣𝐢𝐝 <chatgpt209.6@gmail.com>
2025-06-26 12:00:33 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const TelegramBot = require('node-telegram-bot-api');
+const config = require('./config.json');
+const startBotLogic = require('./xass');
+
+const bot = new TelegramBot(config.token, { polling: true });
+
+bot.on('polling_error', (error) => {
+  console.error('Polling error:', error.code, error.message);
+});
+
+startBotLogic(bot, config);
